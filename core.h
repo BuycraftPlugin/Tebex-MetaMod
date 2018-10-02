ad44b87220f80c8df9674a5977d6cdf4a326b36c
Liam Wiltshire <liam@w.iltshi.re>
2018-10-02 14:47:38 +0100

    Some inital boilerplate code

---
+++ core.h
@@ -0,0 +1,6 @@
+#ifndef _CORE_H
+#define _CORE_H
+
+#include "dllinfo.h"
+
+#endif //_CORE_H
---
+++ dllinfo.h
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+#define VDATE               "02.10.2018"
+#define VVERSION            "0.1"
+
+#define VNAME               "Tebex-MetaMod"
+#define VAUTHOR             "Tebex"
+#define VURL                "https://www.tebex.io"
+#define VLOGTAG             "TEBEXMETAMOD"
+
+#define RC_VERS_DWORD       1,0,1,0
+#define RC_COMMENTS         "Tebex MetaMod"
+#define RC_DESC             "Official MetaMod plugin for the Tebex monetization platform"
+#define RC_INTERNAL         VLOGTAG
+#define RC_COPYRIGHT        "Copyright (c) " VAUTHOR
+#define RC_FILENAME         "tebex-metamod.dll"
+////////////////////////////////////////////////////////////////////
+
